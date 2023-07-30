25b30e2cdb4b172490afe97341c9b490d6f57bdd
1hhhussien1 <122138833+1hhhussien1@users.noreply.github.com>
2023-07-30 18:08:17 +0000

    Hw2 completed

--- hw2/data.js
+++ hw2/data.js
@@ -36,40 +36,10 @@ const items = [
   }
 ];
 
-const getAll = () => {
+export const getAll = () => {
   return items;
 };
 
-const getItem = (id) => {
-  return items.find((item) => item.id === id);
+export const getItem = (id) => {
+  return items.find((item) => item.id === Number(id));
 };
-
-module.exports = { getAll, getItem };
-
-
-//index.js
-
-const express = require('express');
-const app = express();
-const data = require('./data.js');
-
-app.set('view engine', 'ejs');
-
-app.get('/', (req, res) => {
-  const items = data.getAll();
-  res.render('home', { items });
-});
-
-app.get('/detail', (req, res) => {
-  const id = parseInt(req.query.id);
-  const item = data.getItem(id);
-  if (item) {
-    res.render('detail', { item });
-  } else {
-    res.status(404).send('Item not found');
-  }
-});
-
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
-});
--- hw2/index.js
+++ hw2/index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-
+import { getAll, getItem } from '../hw2/data.js'; 
 const app = express();
 app.set ('port', process.env.PORT || 3000);
 app.use(express.static('pubic'));
@@ -7,9 +7,12 @@ app.set('view engine', 'ejs');
 
 app.get('/', (req,res) => {
   console.log(req.url)
-  res.render('home', {name: req.query.name});
+  res.render('home', {cars: getAll()});
 });
-
+app.get('/cars/:id', (req,res) => {
+  console.log(req.url)
+  res.render('detail', {car: getItem(req.params.id)});
+});
 app.get('/about', (req,res) => {
   console.log(req.url)
   res.send('This class is about making great web sites');
@@ -23,40 +26,5 @@ app.use((req,res) => {
 app.listen(app.get('port'), () => {
   console.log('Express started'); 
 });
-app.get('/foo', (req,res,next) => {
-       if(Math.random() < 0.5) return next();
-       res.send('sometimes this');
-});
-app.get('/foo', (req,res) => {
-       res.send('and sometimes that');
-});
-app.get('/user(name)?', (req,res) => {
-       res.render('user');
-});
-app.get('/user/:name', (req, res) => {
 
-       let info = users.find((user) => {
-
-           return user.name = req.params.name;
-
-       })
-
-       if(!info) return next();        // will eventually fall through to 404
-
-       res.send(info);
-
-})
-import routes from './index.js';
-const app_routes = routes(app); // passes ‘app’ instance to the routes module
-export default = (app) => {
-       app.get('/', (req,res) => {
-               app.render('home');
-       
-       //… other routes
-});
-app.get('/get', (req,res) => {
-  console.log(req.query); // display parsed querystring object
-});
-app.post('/get', (req,res) => {
-  console.log(req.body); // display parsed form submission
-})};
+      
